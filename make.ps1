14bc2a98ab542db3e015550447c048987a3695fa
ShiniShiho <nakxkri@shinishiho.cyou>
2024-06-04 14:30:23 +0700

    F me

--- make.ps1
+++ make.ps1
@@ -1,3 +1,3 @@
-gcc -o src/tictactoe.exe src/tictactoe.c myUtils.c -Wall -Wextra -std=c17  -I src/headers -L src/lib -lraylib -lgdi32 -lwinmm -lopengl32
-gcc -o src/minesweeper.exe src/minesweeper.c myUtils.c -Wall -Wextra -std=c17  -I src/headers  -L src/lib -lraylib -lgdi32 -lwinmm -lopengl32
-gcc -o src/game.exe main.c src/myUtils.c -Wall -Wextra -std=c17  -I src/headers -L src/lib -lraylib -lgdi32 -lwinmm -lopengl32
+gcc -o tictactoe.exe src/tictactoe.c src/myUtils.c -Wall -Wextra -std=c17  -I src/headers -L src/lib -lraylib -lgdi32 -lwinmm -lopengl32
+gcc -o minesweeper.exe src/minesweeper.c src/myUtils.c -Wall -Wextra -std=c17  -I src/headers  -L src/lib -lraylib -lgdi32 -lwinmm -lopengl32
+gcc -o game.exe src/main.c src/myUtils.c -Wall -Wextra -std=c17  -I src/headers -L src/lib -lraylib -lgdi32 -lwinmm -lopengl32
